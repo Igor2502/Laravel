e86a4f4b1326c9ce842db51a1daf800955c3f314
Igor2502 <igor.gds.00@gmail.com>
2019-07-06 10:31:19 -0300

    Laravel

--- rotas/routes/web.php
+++ rotas/routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -75,16 +66,59 @@ Route::get('/hellonome/{nome}/{sobrenome}', function($nome, $sn){
                 'sobrenome' => $sn]);
 });
 
+Route::get('/rest/hello', function() {
+    return "Hello (GET)";
+});
 
+Route::post('/rest/hello', function() {
+    return "Hello (POST)";
+});
 
+Route::delete('/rest/hello', function() {
+    return "Hello (DELETE)";
+});
 
+Route::put('/rest/hello', function() {
+    return "Hello (PUT)";
+});
 
+Route::patch('/rest/hello', function() {
+    return "Hello (PATCH)";
+});
 
+Route::options('/rest/hello', function() {
+    return "Hello (OPTIONS)";
+});
 
+Route::post('/rest/imprimir', function(Request $req) {
+    $nome = $req->input('nome');
+    $idade = $req->input('idade');
 
+    return "Hello $nome ($idade)!! (POST)";
+});
 
+Route::match(['get', 'post'], '/rest/hello2', function(){
+    return "Hello world 2";
+});
 
+Route::any('/rest/hello3', function(){
+    return "Hello world 3";
+});
 
+Route::get('/produtos', function(){
+    echo "<h1>PRODUTOS</h1>";
+    echo "<ol>";
+    echo "<li>Notebook</li>";
+    echo "<li>Impressora</li>";
+    echo "<li>Mouse</li>";
+    echo "</ol>";
+})->name('meusprodutos');
 
+Route::get('/linkprodutos', function(){
+    $url = route('meusprodutos');
+    echo "<a href=\"" . $url . "\">Meus produtos</a>";
+});
 
-
+Route::get("/redirecionarprodutos", function(){
+    return redirect()->route('meusprodutos');
+});
