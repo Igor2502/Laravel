42605b80cfbdd3c4ea396724836afa092b409186
Igor2502 <igor.gds.00@gmail.com>
2019-07-06 14:55:38 -0300

    CriandoControladoresComResources

--- construtores/app/Http/Controllers/MeuControlador.php
+++ construtores/app/Http/Controllers/MeuControlador.php
@@ -9,4 +9,19 @@ class MeuControlador extends Controller
     public function getNome() {
         return "Jose da Silva";
     }
+
+    public function getIdade() {
+        return "20 anos";
+    }
+
+    public function multiplicar($n1, $n2) {
+        return $n1 * $n2;
+    }
+
+    public function getNomeById($id) {
+        $v = ['Mario', 'Edson', 'Roberto', 'Jean'];
+        if($id >= 0 && $id < count($v))
+            return $v[$id];
+        return "Não encontrado";
+    }
 }
--- construtores/routes/web.php
+++ construtores/routes/web.php
@@ -5,3 +5,13 @@ Route::get('/', function () {
 });
 
 Route::get('/nome', 'MeuControlador@getNome');
+
+Route::get('/idade', 'MeuControlador@getIdade');
+
+Route::get('/multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
+
+Route::get('/nomes/{id}', 'MeuControlador@getNomeById');
+
+Route::resource('/cliente', 'ClienteControlador');
+
+Route::post('/cliente/requisitar', 'ClienteControlador@requisitar');
