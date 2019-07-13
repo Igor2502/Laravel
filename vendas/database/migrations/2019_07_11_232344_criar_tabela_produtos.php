b934271f3deaba32b66204e83e9378507ea14f01
Igor2502 <igor.gds.00@gmail.com>
2019-07-13 14:28:52 -0300

    Adicionando campos

--- vendas/database/migrations/2019_07_11_232344_criar_tabela_produtos.php
+++ vendas/database/migrations/2019_07_11_232344_criar_tabela_produtos.php
@@ -14,7 +14,7 @@ class CriarTabelaProdutos extends Migration
     public function up()
     {
         Schema::create('produtos', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('nome');
             $table->float('preco');
             $table->integer('estoque');
