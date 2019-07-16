c6773c9963a50a9ca0fc606b9b93071eda2c2236
Igor2502 <igor.gds.00@gmail.com>
2019-07-16 18:45:56 -0300

    Utilizando 'where'

--- modelos/routes/web.php
+++ modelos/routes/web.php
@@ -50,3 +50,33 @@ Route::get('/remover/{id}', function($id) {
         echo "<h1>Categoria não encontrada</h1>";
     }    
 });
+
+Route::get('/categoriapornome/{nome}', function($nome) {
+    $cat = Categoria::where('nome', $nome)->get();
+    foreach($cat as $c){
+        echo "id: " . $c->id . ", ";
+        echo "Nome: " . $c->nome . "<br>";
+    }  
+});
+
+Route::get('/categoriaidmaiorque/{id}', function($id) {
+    $cat = Categoria::where('id', '>', $id)->get();
+    foreach($cat as $c){
+        echo "id: " . $c->id . ", ";
+        echo "Nome: " . $c->nome . "<br>";
+    }  
+
+    $count = Categoria::where('id', '>', $id)->count();
+    echo "<h1>Count: $count </h1>";
+
+    $max = Categoria::where('id', '>', $id)->max('id');
+    echo "<h1>Max: $max </h1>";
+});
+
+Route::get('/ids123', function() {
+    $cat = Categoria::find([1, 2, 3]);
+    foreach($cat as $c){
+        echo "id: " . $c->id . ", ";
+        echo "Nome: " . $c->nome . "<br>";
+    }  
+});
