f88cfdace184944adc3edc9f97d2095cf0314007
Igor2502 <igor.gds.00@gmail.com>
2019-07-27 15:01:34 -0300

    Criando ambiente para validação de formulários

---
+++ validacao/resources/views/novocliente.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Cadastro de Cliente</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+    </head>
+    <body>
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    </body>
+</html>
---
+++ validacao/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('/novocliente', function () {
+    return view('novocliente');
+});
